f43c56711d484294c87ea3306145bac266c0ef76
douglasgvr <douglasfarmaangra@gmail.com>
2025-08-01 12:35:49 -0300

    feat: adicionar seção "Nossas Delícias" com cards e estilos

---
+++ src/app/_components/category-cards/category-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-cards',
+  imports: [],
+  templateUrl: './category-cards.html',
+  styleUrl: './category-cards.scss'
+})
+export class CategoryCards {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './_components/header/header';
 import { Hero } from './_components/hero/hero';
 import { WelcomeMessage } from './_components/welcome-message/welcome-message';
+import { CategoryCards } from './_components/category-cards/category-cards';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, WelcomeMessage],
+  imports: [RouterOutlet, Header, Hero, WelcomeMessage, CategoryCards],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
