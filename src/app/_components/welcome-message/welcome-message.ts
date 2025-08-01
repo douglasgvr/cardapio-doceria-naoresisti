114d5fc1620a4469319672bd1cdfc5896ed23cbc
douglasgvr <douglasfarmaangra@gmail.com>
2025-07-31 21:13:27 -0300

    feat: cria HeroComponent com mensagens de boas-vindas e transição

---
+++ src/app/_components/welcome-message/welcome-message.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-message',
+  imports: [],
+  templateUrl: './welcome-message.html',
+  styleUrl: './welcome-message.scss'
+})
+export class WelcomeMessage {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './_components/header/header';
 import { Hero } from './_components/hero/hero';
+import { WelcomeMessage } from './_components/welcome-message/welcome-message';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, WelcomeMessage],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
